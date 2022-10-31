19eee93044d547f29a872a584cf69e717cbb503f
Cobie French <chf19@fsu.edu>
2022-10-31 13:12:39 -0400

    Rain
    Co-authored-by: Rob Duarte <me@robduarte.com>

--- sketch.js
+++ sketch.js
@@ -1,24 +1,26 @@
-// input stuck to cloud
 // rain animation
 // flower growing
 // cloud changing color
 // rain based on value of input
 
-var cnv;
+var cnv,inp;
+var stage = "typing";
 
 function centerCanvas() {
   var x = (windowWidth - width) / 2;
   var y = (windowHeight - height) / 2;
   cnv.position(x, y);
+  inp.position(x + width/2 - 100,y + height/2 - 215);
 }
 
 function setup() {
   cnv = createCanvas(500, 700);
-  centerCanvas();
-  let inp = createInput();
-  inp.position(275,250);
+  inp = createInput();
+  inp.position(width/2,height/2);
 inp.size(200, 40);
 inp.input(myInputEvent);
+centerCanvas();
+rectMode(CENTER);
 }
 
 function myInputEvent() {
@@ -29,16 +31,36 @@ function windowResized() {
   centerCanvas();
 }
 
+let enterTime
+
 function draw() {
   background(106, 175, 184);
+  if (stage == "raining") {
+    fill(46, 89, 158)
+    rect(width/2, frameCount, 200, 400)
+    if (millis() - enterTime >= 3000) {
+      stage = "flowering"
+    }
+  }
+  fill(106, 175, 184)
+  rect(width/2, 0, 500, 320)
   fill(81, 148, 88)
   noStroke()
-  rect(0, 625, 500, 75)
-  fill(255,255,255)
+  rect(width/2, 665, 500, 75)
+  fill(255)
   ellipse(250, 125, 125)
   ellipse(200, 150, 125)
   ellipse(300, 150, 125)
   ellipse(350, 161, 100)
-  ellipse(140, 161, 100)
-  ellipse(260, 161, 100)
+  ellipse(150, 161, 100)
+  ellipse(250, 161, 100)
+}
+
+function keyPressed() {
+  if (keyCode === ENTER) {
+    stage = "raining"
+    // record the current timestamp (millis()) in a variable called enterPressedTimestamp
+    enterTime = millis();
+
+  }
 }
